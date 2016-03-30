8ad3a15b3d42829fa4fe78ccaa563e083d0ac5c0
Rahul John Louis <rahul.louis@gmail.com>
2016-03-30 19:09:20 -0400

    @Qualifier

--- spring-tutorial3/src/main/java/com/caveofprogramming/spring/test/Logger.java
+++ spring-tutorial3/src/main/java/com/caveofprogramming/spring/test/Logger.java
@@ -1,35 +1,31 @@
 package com.caveofprogramming.spring.test;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.beans.factory.annotation.Qualifier;
 
 public class Logger {
 
-	@Autowired
-	private LogWriter consoleWriter;
-	@Autowired
+	private ConsoleWriter consoleWriter;
 	private LogWriter fileWriter;
 
-	
-	
-//	public void setConsoleWriter(LogWriter consoleWriter) {
-//		this.consoleWriter = consoleWriter;
-//	}
-	
-	
-//	public void setFileWriter(LogWriter fileWriter) {
-//		this.fileWriter = fileWriter;
-//	}
-	
-	public void writeFile(String text)
-	{
+	@Autowired
+	@Qualifier("toconsole")
+	public void setConsoleWriter(ConsoleWriter consoleWriter) {
+		this.consoleWriter = consoleWriter;
+	}
+
+	@Autowired
+	@Qualifier("filewriter")
+	public void setFileWriter(LogWriter fileWriter) {
+		this.fileWriter = fileWriter;
+	}
+
+	public void writeFile(String text) {
 		fileWriter.write(text);
 	}
-	
-	public void writeConsole(String text)
-	{
-		consoleWriter.write(text);
-	}
-	
-	
 
+	public void writeConsole(String text) {
+		if(consoleWriter!=null)
+			consoleWriter.write(text);
+	}
 }
